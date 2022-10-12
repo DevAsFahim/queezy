57431e9ef9883c00ac4951b800766c0fb257726e
ASFahim007 <devasfahim@gmail.com>
2022-10-12 21:24:24 +0600

    full website completed

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h4>Error darling!</h4>
+        <div className='text-center pt-5 mt-5'>
+            <h1>404 Error</h1>
+            <p className='mb-1'>Something went wrong</p>
+            <p>page not found</p>
         </div>
     );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg bg-light py-3">
             <div className="container">
                 <Link className="navbar-brand" to="/">Queezy</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,13 +13,13 @@ const Header = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            <NavLink className={ ({isActive}) => isActive ? 'activeLink' : undefined } aria-current="page" to="/">Home</NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/statistic">Statistic</Link>
+                            <NavLink className={ ({isActive}) => isActive ? 'activeLink' : undefined } to="/statistic">Statistic</NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/blog">Blog</Link>
+                            <NavLink className={ ({isActive}) => isActive ? 'activeLink' : undefined } to="/blog">Blog</NavLink>
                         </li>
                     </ul>
                 </div>
