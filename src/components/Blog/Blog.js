16412e3ce69082072a8b048020c667a73c38ac6d
ASFahim007 <devasfahim@gmail.com>
2022-10-12 15:54:20 +0600

    blog text added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -10,28 +10,25 @@ const Blog = () => {
             <div className="container">
                 <div className="box-container">
                     <div className="blog">
-                        <h1 className="blog_title">1. How react js work</h1>
+                        <h1 className="blog_title">1. What is the purpose of react router</h1>
                         <div className="blog_image">
                             {/* <img src={react} alt="" /> */}
                         </div>
-                        <p className="blog_text mt-4">React is a declarative, efficient, and flexible JavaScript library for building user interfaces. ‘V’ denotes the view in MVC. ReactJS is an open-source, component-based front end library responsible only for the view layer of the application. It is maintained by Facebook. React uses a declarative paradigm that makes it easier to reason about your application and aims to be both efficient and flexible. It designs simple views for each state in your application, and React will efficiently update and render just the right component when your data changes. The declarative view makes your code more predictable and easier to debug.
-                        </p>
+                        <p className="blog_text mt-4">ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route. React Router is a standard library system built on top of the React and used to create routing in the React application using React Router Package. It provides the synchronous URL on the browser with data that will be displayed on the web page. It maintains the standard structure and behavior of the application and mainly used for developing single page web applications.</p>
                     </div>
                     <div className="blog">
-                        <h1 className="blog_title">2. Difference between props and state in react</h1>
+                        <h1 className="blog_title">2. How does context API works</h1>
                         <div className="blog_image">
                             {/* <img src={props} alt="" /> */}
                         </div>
-                        <p className="blog_text mt-4">Props are used to pass data, whereas state is for managing data. Data from props is read-only, and cannot be modified by a component that is receiving it from outside. State data can be modified by its own component, but is private (cannot be accessed from outside). React allows us to pass information to a Component using something called props (stands for properties). Props are objects which can be used inside a component. We will learn about these in detail in this article. Passing and Accessing props. We can pass props to any component as we declare attributes for any HTML tag. The state is a built-in React object that is used to contain data or information about the component. A component's state can change over time; whenever it changes, the component re-renders.
-                        </p>
+                        <p className="blog_text mt-4">The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux. React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state. More information can be found on React's documentation page</p>
                     </div>
                     <div className="blog">
                         <h1 className="blog_title">3. what does useeffect do rather than loading data with api</h1>
                         <div className="blog_image">
                             {/* <img src={useeffect} alt="" /> */}
                         </div>
-                        <p className="blog_text mt-4">
-                        By using this Hook, you tell React that your component needs to do something after render. React will remember the function you passed (we'll refer to it as our “effect”), and call it later after performing the DOM updates. In this effect, we set the document title, but we could also perform data fetching or call some other imperative API. Placing useEffect inside the component lets us access the count state variable (or any props) right from the effect. We don’t need a special API to read it — it’s already in the function scope. Hooks embrace JavaScript closures and avoid introducing React-specific APIs where JavaScript already provides a solution.
+                        <p className="blog_text mt-4">Essentially, useRef is like a “box” that can hold a mutable value in its .current property. You might be familiar with refs primarily as a way to access the DOM. If you pass a ref object to React with, React will set its .current property to the corresponding DOM node whenever that node changes. This works because useRef() creates a plain JavaScript object. The only difference between useRef() and creating a object yourself is that useRef will give you the same ref object on every render.
                         </p>
                     </div>
                 </div>
